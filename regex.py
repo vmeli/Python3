51bae959d35f35ccc7a0e20f542f9e088fe41751
vceli <vickensi.delacruz@gmail.com>
2017-11-12 14:07:42 -0500

    regex-examples

--- regex.py
+++ regex.py
@@ -1,7 +1,14 @@
 import re
-#print(re.search(r'e', 'eucaliptoeucalicto'))
+print(re.search(r'e', 'eucaliptoeucalicto'))
+
 pattern_number = re.compile('\d\d')
-#print(pattern.search('affafafa22fafaf33'))
+print(pattern_number.search('affafafa22fafaf33'))
 
 pattern_uppercase = re.compile('[A-Z]\d')
 print(pattern_uppercase.search('afafafafaAFAFFA4'))
+
+t = re.search('[A-Z][0-9].*$', 'afafaf AF1afa afafa')
+if (t):
+    print('Se encontró [A-Z][0-9].*$', t)
+
+print(re.sub(r'\d', '-', 'la2notica3de6hoy#@LR'))
